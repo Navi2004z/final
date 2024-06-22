5589763b4e352699e3fb1d26cf5d371ef2c257c5
Navi2004z <naveenasandhiya0803@gmail.com>
2024-06-22 22:08:21 +0530

    created html files

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function myfunc(){
+    const a=parseInt(document.getElementById("num").value)
+const b=parseInt(document.getElementById("nun").value)
+    document.getElementById("result").innerHTML = a+b;
+}
+function myfunc1(){
+    const a=document.getElementById("num").value
+const b=document.getElementById("nun").value
+    document.getElementById("result").innerHTML = a-b;
+}
+function myfunc2(){
+    const a=document.getElementById("num").value
+const b=document.getElementById("nun").value
+    document.getElementById("result").innerHTML = a*b;
+}
+function myfunc3(){
+    const a=document.getElementById("num").value
+const b=document.getElementById("nun").value
+    document.getElementById("result").innerHTML = a/b;
+}
